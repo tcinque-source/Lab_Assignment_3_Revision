d8de74ab6d0445eaa92829d519e26c985454bd85
tcinque-source <tcinque@me.bergen.edu>
2026-07-15 16:40:03 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,16 +3,19 @@ public class Main {
  
     public static String paragraph;
     public static String targetWord;
-    private static String delimiter = ".!?";
+    private static String delimiter = ".!?"; //sentence terminating punctuation
     
     public static void main(String[] args) {
+        //takes user input for typed paragraph
         Scanner keyboard = new Scanner(System.in);
         paragraph = keyboard.nextLine();
         
+        //object for words
         TextAnalyzer ta = new TextAnalyzer(paragraph);
         
         System.out.println("Total Words: " + ta.countWords());
         
+        //object for sentences
         TextAnalyzer _ta = new TextAnalyzer(paragraph, delimiter);    
         
         System.out.println("Total Sentences: " + _ta.countSentences());
